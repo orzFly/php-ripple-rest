cae3484b7369a56a880ccac93304bc019ec8ecc8
Yeechan Lu <wz.bluesnow@gmail.com>
2014-05-02 14:09:11 +0800

    Fix address

--- test_example.php
+++ test_example.php
@@ -20,7 +20,7 @@ var_dump($account->getTrustlines());
 # $account->addTrustline("5/ICE/r4H3F9dDaYPFwbrUsusvNAHLz2sEZk4wE5");
 
 var_dump($account->getNotification("DD9F40516152090612B12F1CCD5A88828AEA8813FEBD56D9D6B39ED918F4CCCA"));
-var_dump($account->findPaymentPaths("rES1hSkoWauMk3r6sgh7zfjpTCnwGbqaxA", "5+ICE+r4H3F9dDaYPFwbrUsusvNAHLz2sEZk4wE5"));
+var_dump($account->findPaymentPaths("rhtgn6PYbXwhA6QJJMY4btieoap31t7Uo8", "5+ICE+rES1hSkoWauMk3r6sgh7zfjpTCnwGbqaxA"));
 var_dump($account->queryPayments(array( "resultsPerPage" => 10 )));
 
 $payment = $account->createPayment("rES1hSkoWauMk3r6sgh7zfjpTCnwGbqaxA", "5+XRP");
